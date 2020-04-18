524a170e8feaf530c00439d26cf182e429268124
stefaleon <stefaleon@gmail.com>
2020-04-18 17:22:34 +0300

    sort by name

--- src/components/Bandlist.js
+++ src/components/Bandlist.js
@@ -1,32 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
 
+const apiUrl = 'http://localhost:5000/api/bands';
+const apiUrlSortByName = 'http://localhost:5000/api/bands?sort=name';
+
 const Bandlist = () => {
-  const [data, setData] = useState({ bands: [] });
+  const [bands, setBands] = useState([]);
+  const [url, setUrl] = useState(apiUrl);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios('http://localhost:5000/api/bands');
+      const result = await axios(url);
       console.log(result.data.data);
-      setData({ bands: result.data.data });
+      setBands(result.data.data);
     };
     fetchData();
-  }, []);
+  }, [url]);
+
+  const sortByName = () => {
+    setUrl(apiUrlSortByName);
+  };
 
   return (
-    <ul>
-      {data.bands.map((band) => (
-        <li key={band.id}>
-          <a
-            href={`http://localhost:5000/uploads/${band.photo}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            {band.name}
-          </a>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <button onClick={sortByName}>Sort by name</button>
+      <ul>
+        {bands.map((band) => (
+          <li key={band.id}>
+            <a
+              href={`http://localhost:5000/uploads/${band.photo}`}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              {band.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 };
 
